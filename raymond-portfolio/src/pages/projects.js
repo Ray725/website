03d9a12be8a16e908efe9d6dabb1d39c06b587df
Ray725@users.noreply.github.com <>
2018-08-21 18:07:10 -0400

    working on updating projects

--- raymond-portfolio/src/pages/projects.js
+++ raymond-portfolio/src/pages/projects.js
@@ -23,14 +23,17 @@ function ProjectPage(props) {
     <div className={classes.root}>
       <Grid container spacing={24}>
         <Grid item xs={12}>
-          <SimpleCard title="Google" date="2001" description="mine" url="https://google.com"></SimpleCard>
+          <SimpleCard title="ViQuote" date="December 2017" description="Web application through which prospective health insurance buyers can get insurance cost predictions by submitting their information to a cloud-based machine-learning model" url="https://devpost.com/software/yhack2k17"></SimpleCard>
         </Grid>
-        <Grid item xs={6}>
-          <SimpleCard title="Google" date="2001" description="mine" url="https://google.com"></SimpleCard>
+        <Grid item xs={12}>
+          <SimpleCard title="20/20 Opioid" date="Fall 2017" description="Exploration tool and visualization of opioid epidemic data." url="https://jasonmohabir.github.io/AOE-opioid/totalOverdose.html"></SimpleCard>
         </Grid>
-        <Grid item xs={6}>
-          <SimpleCard title="Google" date="2001" description="mine" url="https://google.com"></SimpleCard>
+        <Grid item xs={12}>
+          <SimpleCard title="HALp Me Read" date="Fall 2017" description="Modified Amazon Alexa that parses the most salient points of research papers using NLP." url="https://devpost.com/software/halp-me-read-qgua3k"></SimpleCard>
       	</Grid>
+        <Grid item xs={12}>
+          <SimpleCard title="Logan High School Peer Tutor System" date="2016" description="Founded a peer tutor program based on online appointment scheduling in high school." url="https://lhspeertutorsystem.herokuapp.com/"></SimpleCard>
+        </Grid>
       </Grid>
     </div>
   );
